65bf064e92e2b4c71d99c5798a9c2fe6a901724e
Sylvain-pro <sidambaroms@gmail.com>
2018-11-26 22:48:32 +0100

    adding new example of arrow function

--- 02_arrow_function/main.ts
+++ 02_arrow_function/main.ts
@@ -1,8 +1,16 @@
 
-let greeting(name: string) => {
+let greeting = (name: string) => {
     return "Hi, " + name;
 }
 
 let newGreeting = (name: string) => {
     return "Hi " + name + ", my friend";
 };
+
+
+
+
+
+var numbers = [1,2,3,4,5,6];
+
+var evens = numbers.filter((value) => value % 2 === 0);
